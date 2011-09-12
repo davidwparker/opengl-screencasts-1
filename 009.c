9e40e6b1f80def34a6323b06afbe08a110cd6b9e
David Parker <davidwparker@gmail.com>
2011-09-12 15:23:27 -0600

    added episode 9

---
+++ 009.c
@@ -0,0 +1,352 @@
+#include "screencasts.h"
+
+/*  Poor man's approximation of PI */
+#define PI 3.1415926535898
+/*  Macro for sin & cos in degrees */
+#define Cos(th) cos(PI/180*(th))
+#define Sin(th) sin(PI/180*(th))
+
+/*  Globals */
+double dim=3.0; /* dimension of orthogonal box */
+char *windowName = "OpenGL screenscasts 9: Rotation, Scaling, and Transformations";
+int windowWidth=500;
+int windowHeight=450;
+
+/*  Various global state */
+int toggleAxes = 1;   /* toggle axes on and off */
+int toggleValues = 1; /* toggle values on and off */
+int toggleMode = 0; /* projection mode */
+int th = 0;   /* azimuth of view angle */
+int ph = 0;   /* elevation of view angle */
+int fov = 55; /* field of view for perspective */
+int asp = 1;  /* aspect ratio */
+
+/*  Cube vertices */
+GLfloat vertA[3] = { 0.5, 0.5, 0.5};
+GLfloat vertB[3] = {-0.5, 0.5, 0.5};
+GLfloat vertC[3] = {-0.5,-0.5, 0.5};
+GLfloat vertD[3] = { 0.5,-0.5, 0.5};
+GLfloat vertE[3] = { 0.5, 0.5,-0.5};
+GLfloat vertF[3] = {-0.5, 0.5,-0.5};
+GLfloat vertG[3] = {-0.5,-0.5,-0.5};
+GLfloat vertH[3] = { 0.5,-0.5,-0.5};
+
+/*
+ * project()
+ * ------
+ * Sets the projection
+ */
+void project() 
+{
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+
+  if (toggleMode) {
+    /* perspective */
+    gluPerspective(fov,asp,dim/4,4*dim);
+  }
+  else {
+    /* orthogonal projection*/
+    glOrtho(-dim*asp,+dim*asp, -dim,+dim, -dim,+dim);
+  }
+
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+}
+
+/*
+ * setEye()
+ * ------
+ * Set the eye position
+ */
+void setEye()
+{
+  if (toggleMode) {
+    double Ex = -2*dim*Sin(th)*Cos(ph);
+    double Ey = +2*dim        *Sin(ph);
+    double Ez = +2*dim*Cos(th)*Cos(ph);
+    /* camera/eye position, aim of camera lens, up-vector */
+    gluLookAt(Ex,Ey,Ez , 0,0,0 , 0,Cos(ph),0);
+  }
+  /*  Orthogonal - set world orientation */
+  else {
+    glRotatef(ph,1,0,0);
+    glRotatef(th,0,1,0);
+  }
+}
+
+/*
+ * drawAxes()
+ * ------
+ * Draw the axes
+ */
+void drawAxes() 
+{
+  if (toggleAxes) {
+    /*  Length of axes */
+    double len = 2.0;
+    glColor3f(1.0,1.0,1.0);
+    glBegin(GL_LINES);
+    glVertex3d(0,0,0);
+    glVertex3d(len,0,0);
+    glVertex3d(0,0,0);
+    glVertex3d(0,len,0);
+    glVertex3d(0,0,0);
+    glVertex3d(0,0,len);
+    glEnd();
+    /*  Label axes */
+    glRasterPos3d(len,0,0);
+    print("X");
+    glRasterPos3d(0,len,0);
+    print("Y");
+    glRasterPos3d(0,0,len);
+    print("Z");
+  }
+}
+
+/*
+ *  drawValues()
+ *  ------
+ *  Draw the values in the lower left corner
+ */
+void drawValues()
+{
+  if (toggleValues) {
+    glColor3f(0.8,0.8,0.8);
+    printAt(5,5,"View Angle (th, ph) =(%d, %d)", th,ph);
+    printAt(5,25,"Projection mode =(%s)", toggleMode?"Perspective":"Orthogonal");
+  }
+}
+
+/*
+ *  drawShape()
+ *  ------
+ *  Draw the GLUT shape
+ */
+void drawShape()
+{
+  /*
+  glTranslated(0,0,0);
+  glRotated(0,0,0,0);
+  glScaled(1,1,1);
+  */
+  
+  /*
+    translation
+  */
+  // x, y, z
+  // x
+  //glTranslated(-0.5,0,0);
+  // y
+  //glTranslated(0,0.5,0);
+  // z
+  //glTranslated(0,0,0.5);
+
+  /*
+    rotation
+  */
+  // degrees, x, y, z
+  // x
+  //glRotated(45,1,0,0);
+  // y
+  //glRotated(45,0,1,0);
+  // z
+  //glRotated(45,0,0,1);
+
+  /*
+    scaling
+  */
+  // x, y, z
+  // x
+  //glScaled(2,1,1);
+  // y
+  //glScaled(1,2,1);
+  // z
+  //glScaled(1,1,2);
+
+  /*
+    order matters
+  */
+  //glTranslated(1,0,0);
+  //glRotated(45,0,1,0);
+  //glScaled(1,1,3);
+  
+  // versus
+  //glScaled(1,1,3);
+  //glRotated(45,0,1,0);
+  //glTranslated(1,0,0);
+
+  /*
+    multiple srt at the same time 
+  */
+  //glRotated(45,1,0,0);
+  //glTranslated(1,0,1);
+
+  //glRotated(45,1,0,1);
+  // versus
+  //glRotated(45,1,0,0);
+  //glRotated(45,0,0,1);
+
+  //glScaled(2,3,0.5);
+
+  /* Cube */
+  glBegin(GL_QUADS);
+  /* front => ABCD yellow */
+  glColor3f(1.0,1.0,0.0);
+  glVertex3fv(vertA);
+  glVertex3fv(vertB);
+  glVertex3fv(vertC);
+  glVertex3fv(vertD);
+  /* back => FEHG red */
+  glColor3f(1.0,0.0,0.0);
+  glVertex3fv(vertF);
+  glVertex3fv(vertE);
+  glVertex3fv(vertH);
+  glVertex3fv(vertG);
+  /* right => EADH green */
+  glColor3f(0.0,1.0,0.0);
+  glVertex3fv(vertE);
+  glVertex3fv(vertA);
+  glVertex3fv(vertD);
+  glVertex3fv(vertH);
+  /* left => BFGC blue */
+  glColor3f(0.0,0.0,1.0);
+  glVertex3fv(vertB);
+  glVertex3fv(vertF);
+  glVertex3fv(vertG);
+  glVertex3fv(vertC);
+  /* top => EFBA turquoise */
+  glColor3f(0.0,1.0,1.0);
+  glVertex3fv(vertE);
+  glVertex3fv(vertF);
+  glVertex3fv(vertB);
+  glVertex3fv(vertA);
+  /* bottom => DCGH pink */
+  glColor3f(1.0,0.0,1.0);
+  glVertex3fv(vertD);
+  glVertex3fv(vertC);
+  glVertex3fv(vertG);
+  glVertex3fv(vertH);
+  glEnd();
+}
+
+/*
+ *  display()
+ *  ------
+ *  Display the scene
+ */
+void display()
+{
+  glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+  glEnable(GL_DEPTH_TEST);
+  glLoadIdentity();
+
+  /* setup functions */
+  setEye();
+
+  /* draw */
+  drawAxes();
+  drawValues();
+
+  /* magic here */
+  drawShape();
+
+  glFlush();
+  glutSwapBuffers();
+}
+
+/*
+ *  reshape()
+ *  ------
+ *  GLUT calls this routine when the window is resized
+ */
+void reshape(int width,int height)
+{
+  asp = (height>0) ? (double)width/height : 1;
+  glViewport(0,0, width,height);
+  project();
+}
+
+/*
+ *  windowKey()
+ *  ------
+ *  GLUT calls this routine when a non-special key is pressed
+ */
+void windowKey(unsigned char key,int x,int y)
+{
+  /*  Exit on ESC */
+  if (key == 27) exit(0);
+  else if (key == 'a' || key == 'A') toggleAxes = 1-toggleAxes;
+  else if (key == 'v' || key == 'V') toggleValues = 1-toggleValues;
+  else if (key == 'm' || key == 'M') toggleMode = 1-toggleMode;
+  /*  Change field of view angle */
+  else if (key == '-' && key>1) fov--;
+  else if (key == '+' && key<179) fov++;
+  /*  Change dimensions */
+  else if (key == 'D') dim += 0.1;
+  else if (key == 'd' && dim>1) dim -= 0.1;
+
+  project();
+  glutPostRedisplay();
+}
+
+/*
+ *  windowSpecial()
+ *  ------
+ *  GLUT calls this routine when an arrow key is pressed
+ */
+void windowSpecial(int key,int x,int y)
+{
+  /*  Right arrow key - increase azimuth by 5 degrees */
+  if (key == GLUT_KEY_RIGHT) th += 5;
+  /*  Left arrow key - decrease azimuth by 5 degrees */
+  else if (key == GLUT_KEY_LEFT) th -= 5;
+  /*  Up arrow key - increase elevation by 5 degrees */
+  else if (key == GLUT_KEY_UP) ph += 5;
+  /*  Down arrow key - decrease elevation by 5 degrees */
+  else if (key == GLUT_KEY_DOWN) ph -= 5;
+
+  /*  Keep angles to +/-360 degrees */
+  th %= 360;
+  ph %= 360;
+
+  project();
+  glutPostRedisplay();
+}
+
+/*
+ *  windowMenu
+ *  ------
+ *  Window menu is the same as the keyboard clicks
+ */
+void windowMenu(int value)
+{
+  windowKey((unsigned char)value, 0, 0);
+}
+
+/*
+ *  main()
+ *  ----
+ *  Start up GLUT and tell it what to do
+ */
+int main(int argc,char* argv[])
+{
+  glutInit(&argc,argv);
+  glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+  glutInitWindowSize(windowWidth,windowHeight);
+  glutCreateWindow(windowName);
+
+  glutDisplayFunc(display);
+  glutReshapeFunc(reshape);
+  glutKeyboardFunc(windowKey);
+  glutSpecialFunc(windowSpecial);
+
+  glutCreateMenu(windowMenu);
+  glutAddMenuEntry("Toggle Axes [a]",'a');
+  glutAddMenuEntry("Toggle Values [v]",'v');
+  glutAddMenuEntry("Toggle Mode [m]",'m');
+  glutAttachMenu(GLUT_RIGHT_BUTTON);
+
+  glutMainLoop();
+  return 0;
+}
--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 # Target to build
-#TARGET = 001 002 003 004 005 006 007 008
-TARGET = 008
-#EXECS = ./executables/001 ./executables/002 ./executables/003 ./executables/004 ./executables/005 ./executables/006 ./executables/007 ./executables/008
-EXECS = ./executables/008
+#TARGET = 001 002 003 004 005 006 007 008 009
+TARGET = 009
+#EXECS = ./executables/001 ./executables/002 ./executables/003 ./executables/004 ./executables/005 ./executables/006 ./executables/007 ./executables/008 ./executables/009
+EXECS = ./executables/009
 
 # Libraries - LINUX
 #LIBS=-lglut -lGLU
